ef9f17c78c17635bda11246f01300324b8add29b
Martin Kuppe <mkuppe@methodinthemadness.eu>
2017-01-22 05:18:32 +0100

    - Victory panel, defeat and victory check

--- Assets/Scripts/VictoryManager.cs
+++ Assets/Scripts/VictoryManager.cs
@@ -15,10 +15,62 @@ public class VictoryManager : Singleton<VictoryManager>
 {
     public GameObject _defeatPanel;
     public Text _defeatText;
+    public GameObject _victoryPanel;
+    private City[] _cities;
+    private float _checkVictoryTimer = 0.0f;
+
+    public void Awake()
+    {
+        _cities = FindObjectsOfType<City>();
+        Time.timeScale = 1.0f;
+    }
+
+    public void Update()
+    {
+        _checkVictoryTimer -= Time.deltaTime;
+        if(_checkVictoryTimer < 0.0f)
+        {
+            _checkVictoryTimer = 1.0f;
+            CheckVictory();
+        }
+    }
 
     public void GameOver( string message )
     {
+        Time.timeScale = 0.0f;
         _defeatPanel.SetActive(true);
         _defeatText.text = message;
+    }
+
+    public void Victory()
+    {
+        Time.timeScale = 0.0f;
+        _victoryPanel.SetActive(true);
+    }
+
+    private void CheckVictory()
+    {
+        int rebels = 0;
+        int loyalists = 0;
+        foreach( City city in _cities )
+        {
+            if( city.IsRebelCity)
+            {
+                rebels++;
+            }
+            else
+            {
+                loyalists++;
+            }
+        }
+
+        if(rebels == 0)
+        {
+            GameOver("You lost the last city.");
+        }
+        else if(loyalists == 0 )
+        {
+            Victory();
+        }
     }
 }
